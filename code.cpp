ee32fd499740e70abb0764634aa82184d46e0d55
bunny-ml <abhishek260204@gmail.com>
2025-04-13 11:25:04 +0530

    commit

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"you have to get a job in next month or in next 2 months!" <<endl;
+    cout<<"changes" <<endl;
+    cout<<"hello";
+    return 0;
+} 
