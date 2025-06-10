e4c37c68e3b780ffa3779a19353a2b1cf58778aa
Abhishek Chaudhary <123234226+bunny-ml@users.noreply.github.com>
2025-06-10 00:50:51 +0000

    commit

--- code1.cpp
+++ code1.cpp
@@ -3,17 +3,30 @@
 
 
 using namespace std;
-int main()
+int main() 
 {
 
-    int  var = 3;
-    bool var2 = true;
-    cout<< var2 <<endl;
+    // int  var = 3;
+    // bool var2 = true;
+    // // int var2 = 3;
+    // cout<< var2 <<endl; //prints var2
 
-
-    printf("Hello world! ""using printf from C"    "\n");
-    cout<<"Hello World! " "using cout" << endl;
+    // if (var == var2){
+    //     printf("var == var2 is true" "\n");
+    // }
+    // else {
+    //     printf( "hello world!" "\n");
+    // }
     
+
+
+    // printf("Hello world! ""using printf from C"  "\n"); //c print method
+
+    // cout<<"Hello World! " "using cout" << endl; // cpp cout method or print 
+    
+    
+
+
 
     return 0;
 
